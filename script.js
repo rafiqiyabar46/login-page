3b1ae89045227088d1392b7333d829f637064c4a
rafiqiyabar46 <rafiqiyabar46@gmail.com>
2025-02-23 15:20:34 +0530

    script.js

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ function login() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     
-    if (username === "yabar" && password === "yabar") {
-        document.getElementById("message").innerText = "Login successful!";
-        document.getElementById("message").style.color = "green";
+    if (username === "yabar" && password === "password") {
+        window.location.href = "welcome.html";  // Redirect to the new page
     } else {
         document.getElementById("message").innerText = "Invalid credentials!";
         document.getElementById("message").style.color = "red";
